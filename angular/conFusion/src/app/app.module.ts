60ad3db6bd819a796e4b94796c8c2cbed15f4369
Matt Maeda <matt@casamaeda.com>
2021-11-17 14:47:29 -0800

    Directives

--- angular/conFusion/src/app/app.module.ts
+++ angular/conFusion/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './shared/baseurl';
 import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
